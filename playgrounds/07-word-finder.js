c0ff019ecbd4fdc286abe6eaae73a58e3dd0af34
Yesid <yesidrodriguez305@gmail.com>
2023-02-07 18:47:30 -0500

    Playground: word finder

---
+++ playgrounds/07-word-finder.js
@@ -0,0 +1,32 @@
+/*
+Tu reto es retornar un array solo con las palabras que cumplan con la condición de tener un término de búsqueda dado.
+
+Para solucionarlo vas a encontrar una función llamada filterByTerm que recibe los siguientes parámetros de entrada:
+
+array: Un array de strigs con palabras
+term: Un string con el término a buscar
+Dentro del cuerpo de la función filterByTerm debes escribir tu solución.
+
+Ejemplo 1:
+
+Input:
+array: ["ana", "santi", "nico", "anastasia"]
+term: "ana"
+
+Ouput:
+["ana", "anastasia"]
+
+Ejemplo 2:
+
+Input:
+array: ["ana", "santi", "nico", "anastasia"]
+term: "xyz"
+
+Output:
+[]
+*/
+
+function filterByTerm(array, term) {
+  // Tu código aquí 👈
+  return array.filter((word) => word.includes(term));
+}
